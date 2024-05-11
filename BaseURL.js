b6168696cb847ff761298981e42953c770ccd942
Ubaidullah Aamir <ubaidullahamir2017@gmail.com>
2024-05-11 10:50:46 -0700

    chore:change BaseURL to deployed url

--- BaseURL.js
+++ BaseURL.js
@@ -1,4 +1,4 @@
-// const BaseURL = "https://melodia-five.vercel.app/api/"
-const BaseURL = "http://localhost:3000/api/"
+const BaseURL = "https://melodia-five.vercel.app/api/"
+// const BaseURL = "http://localhost:3000/api/"
 
 export default BaseURL
